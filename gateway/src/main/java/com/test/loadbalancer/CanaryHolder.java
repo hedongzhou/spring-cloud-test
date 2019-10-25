09d8b55916dc67f22b0162cecc602c7ad7215272
hedongzhou <hedongzhou_job@163.com>
2019-10-25 11:54:04 +0800

    增加zuul金丝雀发布支持

--- gateway/src/main/java/com/test/loadbalancer/CanaryHolder.java
+++ gateway/src/main/java/com/test/loadbalancer/CanaryHolder.java
@@ -1,5 +1,11 @@
 package com.test.loadbalancer;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * 金丝雀名称
  *
@@ -8,16 +14,62 @@ package com.test.loadbalancer;
  */
 public class CanaryHolder {
 
-    private static final ThreadLocal<String> CANARY = new InheritableThreadLocal<>();
+    private static final ThreadLocal<Map<String, String>> CANARY = new InheritableThreadLocal<>();
 
-    public static String get() {
-        return CANARY.get();
+    /**
+     * 是否为空
+     *
+     * @return
+     */
+    public static boolean isEmpty() {
+        Map<String, String> canaryMap = CANARY.get();
+        return canaryMap == null || canaryMap.isEmpty();
     }
 
+    /**
+     * 是否包含服务
+     *
+     * @param serviceName
+     * @return
+     */
+    public static boolean contains(String serviceName) {
+        if (isEmpty()) {
+            return false;
+        }
+
+        return CANARY.get().containsKey(serviceName);
+    }
+
+    /**
+     * 获取目标
+     *
+     * @param serviceName
+     * @return
+     */
+    public static String get(String serviceName) {
+        return isEmpty() ? null : CANARY.get().get(serviceName);
+    }
+
+    /**
+     * 设置信息
+     *
+     * @param canary
+     */
     public static void set(String canary) {
-        CANARY.set(canary);
+        clear();
+
+        canary = StringUtils.trim(canary);
+        if (StringUtils.isNotBlank(canary)) {
+            Map<String, String> canaryMap = Arrays.stream(StringUtils.split(canary, ";"))
+                    .map(str -> StringUtils.split(str, ","))
+                    .collect(Collectors.toMap(arr -> arr[0], arr -> arr[1], (a, b) -> a));
+            CANARY.set(canaryMap);
+        }
     }
 
+    /**
+     * 清理
+     */
     public static void clear() {
         CANARY.remove();
     }
--- gateway/src/main/java/com/test/loadbalancer/CanaryServerPredicate.java
+++ gateway/src/main/java/com/test/loadbalancer/CanaryServerPredicate.java
@@ -5,12 +5,8 @@ import com.netflix.loadbalancer.AbstractServerPredicate;
 import com.netflix.loadbalancer.PredicateKey;
 import com.netflix.loadbalancer.Server;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 金丝雀服务选择
@@ -46,23 +42,18 @@ public class CanaryServerPredicate extends AbstractServerPredicate {
 
     @Override
     public boolean apply(PredicateKey predicateKey) {
-        String canary = CanaryHolder.get();
-        if (StringUtils.isBlank(StringUtils.trim(canary))) {
+        if (CanaryHolder.isEmpty()) {
             return true;
         }
-
-        Map<String, String> canaryMap = Arrays.stream(StringUtils.split(canary, ";"))
-                .map(str -> StringUtils.split(str, ","))
-                .collect(Collectors.toMap(arr -> arr[0], arr -> arr[1], (a, b) -> a));
 
         NacosServer server = (NacosServer) predicateKey.getServer();
         String serviceName = server.getInstance().getServiceName();
         serviceName = serviceName.substring(serviceName.lastIndexOf("@") + 1);
-        if (!canaryMap.containsKey(serviceName)) {
+        if (!CanaryHolder.contains(serviceName)) {
             return true;
         }
 
-        boolean pass = check(server, canaryMap.get(serviceName));
+        boolean pass = check(server, CanaryHolder.get(serviceName));
         if (pass) {
             log.info("hit canary: {}", server);
             return true;
